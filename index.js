2b544a4c6276fad18b46b6d7359c5d423c41a7b3
Jon Abrams <jon@jonabrams.com>
2016-01-23 19:32:09 -0800

    Let's get knocking…

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_SECRET
+});
+
+// TODO: Make a bot
